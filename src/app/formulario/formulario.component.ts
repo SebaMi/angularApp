7a79acaacdf1ed15e35a4d100073ef829eb2f7cd
SebasAKD <sebastian.minici@gmail.com>
2018-11-12 21:56:58 -0300

    clase 2

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+  public nombre: string = '';   // definimos atributos
+  public apellido: string = '';
+  public idNacionalidad: number = 1 ;
+
+  public nacionalidades = [
+    { id: "1", nacionalidad: 'argentino'},
+    { id: "2", nacionalidad: 'brasilero'},
+    { id: "3", nacionalidad: 'uruguayo'}
+  ];
+
+  constructor() { 
+    this.nombre = 'Nombre inicial';   
+    this.apellido = 'Apellido inicial'; 
+
+  }
+
+  ngOnInit() {
+  }
+
+  public enviar() :void {
+    console.log("Se recibio nombre: " + this.nombre + " apellido: " +this.apellido + "nacionalidad: " + this.idNacionalidad);
+  }
+}
